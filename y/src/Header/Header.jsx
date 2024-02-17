fede6768256187d90cc70414336ef4a330f58b8d
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2024-02-17 17:23:32 -0300

    sas

--- y/src/Header/Header.jsx
+++ y/src/Header/Header.jsx
@@ -4,34 +4,46 @@ import styles from './Header.module.css'
 function Header () {
 
     const [vida, setVida] = useState(100);
-    const [vidaAtual, setVidaAtual] = useState();
+    const [vidaAtual, setVidaAtual] = useState(25);
     const [danoEntrada, setDanoEntrada] = useState();
-    const [danoInput, setDanoInput] = useState();
+    const [danoInput, setDanoInput] = useState(10);
 
     function handleDanoInput(event){
         setDanoInput(event.target.value);
     }
 
     function calcularDanoCura(i){
-        const vidaPctg = vidaAtual/100;
-
+        let vidaPctg = vida/100;
+        let j = 1;
+        let tamBarra = "";
+        //const vidaVar = document.getElementsByClassName(styles.vida)
+        setDanoEntrada(danoInput);
+        setVidaAtual(vidaAtual+(danoEntrada*i));
         
+        do{
+            j++;
+            vidaPctg = vidaPctg * j;
+        } while(vidaPctg <= vidaAtual);
+
+        tamBarra = String(j)+"%"
+        console.log(tamBarra);
+        //vidaVar.styles.width = '${tamBarra}'; 
     }
 
     return(
         <div className={styles.container}>
             <div className={styles.vidaContainer}>
-                <div id='vidaAtual' className={styles.vida}></div>
+                <div id='vidaAtual' className={styles.vida}>{vidaAtual}</div>
             </div>
             <input  className={styles.entradaDano}
                     type="number"
                     value={danoInput} 
                     onChange={handleDanoInput}/>
             <div className={styles.botoes}>
-                <button className={styles.botaoDano}>
+                <button className={styles.botaoDano} onClick={calcularDanoCura(-1)}>
                     DANO
                 </button>
-                <button className={styles.botaoCura}>
+                <button className={styles.botaoCura} onClick={calcularDanoCura(1)}>
                     CURA
                 </button>
             </div>
